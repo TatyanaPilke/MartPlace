bb5e99276d513df3bea31261105f614b18805c13
TatyanaPilke <kykla.mc2@gmail.com>
2020-09-13 16:41:49 +0300

    added 10 homework

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function(){
 
-  
+  $(".featured__stars").rateYo({
+    rating: 4.5,
+    starWidth: "13px",
+    readOnly: true
+  });
+
+  $('.featured__inner').slick({
+    arrows: true,
+    fade: true,
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/chevron-left.png" alt="prev arrow"></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/chevron-right.png" alt="next arrow"></button>',
+  })
+
+  var mixer = mixitup('.products__inner');
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ let gulp = require('gulp'),
     cssmin = require('gulp-cssmin');  
 
 gulp.task('sass', function(){
-  return gulp.src('app/scss/style.scss')
+  return gulp.src('app/scss/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(rename({suffix : '.min'}))
         .pipe(autoprefixer({
@@ -22,21 +22,24 @@ gulp.task('style', function(){
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
-    'node_modules/magnific-popup/dist/magnific-popup.css'
+    'node_modules/magnific-popup/dist/magnific-popup.css',
+    'node_modules/rateyo/src/jquery.rateyo.css',
   ])
-   .pipe(concat('libs.min.css'))
-   .pipe(cssmin())
-   .pipe(gulp.dest('app/css'))
+        .pipe(concat('libs.min.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest('app/css'))
 });
 
 gulp.task('script', function(){
    return gulp.src([
-     'node_modules/slick-carousel/slick/slick.js',
-     'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
-   ])
-    .pipe(concat('libs.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('app/js'))
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+    'node_modules/mixitup/dist/mixitup.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
+  ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js'))
 });
  
 gulp.task('html', function() {
@@ -58,7 +61,7 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+  gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
   gulp.watch('app/*.html', gulp.parallel('html'))
   gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
