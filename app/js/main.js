73a4b9cf5de188fb1dd54b3bb07e56a6842b5237
TatyanaPilke <kykla.mc2@gmail.com>
2020-09-22 19:19:59 +0300

    added 10 lessons-change

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,16 @@
 $(function(){
 
-  $(".featured__stars").rateYo({
+  $(".featured-card__stars").rateYo({
+    rating: 4.5,
+    starWidth: "15px",
+    spacing   : "3px",
+    readOnly: true
+  });
+
+  $(".product-card__stars").rateYo({
     rating: 4.5,
     starWidth: "13px",
+    spacing   : "3px",
     readOnly: true
   });
 
@@ -13,6 +21,15 @@ $(function(){
     nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/chevron-right.png" alt="next arrow"></button>',
   })
 
-  var mixer = mixitup('.products__inner');
+  $('.followers__box').slick({
+    arrows: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+    // prevArrow: '<button class="followers-arrow followers-prev"><img src="images/icons/chevron-left.png" alt="prev arrow"></button>',
+    // nextArrow: '<button class="followers-arrow follorwers-next"><img src="images/icons/chevron-right.png" alt="next arrow"></button>',
+  })
+
+  var mixer = mixitup('.new-products__inner');
 
 });
